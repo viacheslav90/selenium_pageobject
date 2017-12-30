44c119532c500d7d8f7f7b6296354e4853b66e11
viacheslav90 <slavka199048@gmail.com>
2017-12-30 15:33:13 +0200

    Update TestData.java

--- src/main/java/config/TestData.java
+++ src/main/java/config/TestData.java
@@ -6,7 +6,7 @@ public class TestData {
     public static final String ENVIRONMENT = "https://rvision-int.exadel.by/";
 
     // Username/password of existing user
-    public static final String EXIST_USERNAME = "Akomolov";
-    public static final String EXIST_PASSWORD = "123";
+    public static final String EXIST_USERNAME = "";
+    public static final String EXIST_PASSWORD = "";
 
 }
